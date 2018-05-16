33912f33b43414ec2d46e0c917f877b19f8713ff
Chiemela Chinedum <chiemelachinedum@gmail.com>
2018-05-16 12:28:29 +0100

    Greet user and alert others when user joins

--- server/server.js
+++ server/server.js
@@ -15,6 +15,18 @@ var io = socketIO(server);
 io.on('connection', (socket) => {
     console.log("New user connected");
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('disconnect', (socket) => {
         console.log("User disconnected");
     });
