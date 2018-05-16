ca3734908cbab3ed84bbd8cbaf9223b01cf62b0e
Chiemela Chinedum <chiemelachinedum@gmail.com>
2018-05-16 15:29:45 +0100

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -10,5 +10,18 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function(message) {
     console.log("New message", message);
+    $("#messages").append(`<li>${message.from}: ${message.text}</li>`);
+});
+
+$("#message-form").on('submit', function(e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'Melas',
+        text: $('[name=message]').val()
+    }, function(data) {
+        console.log('Got it');
+        console.log(data);
+    });
 });
 
